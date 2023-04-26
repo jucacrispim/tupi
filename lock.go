f58ed2be6be34a84262fa32fb0d97b7f5889660b
Juca Crispim <jucacrispim@gmail.com>
2023-04-26 17:27:57 -0300

    Fix dead lock

--- lock.go
+++ lock.go
@@ -1,20 +1,3 @@
-// Copyright 2020 Juca Crispim <juca@poraodojuca.net>
-
-// This file is part of tupi.
-
-// tupi is free software: you can redistribute it and/or modify
-// it under the terms of the GNU Affero General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-
-// tupi is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU Affero General Public License for more details.
-
-// You should have received a copy of the GNU Affero General Public License
-// along with tupi. If not, see <http://www.gnu.org/licenses/>.
-
 package main
 
 import "sync"
@@ -39,7 +22,10 @@ func (km *keyedMutex) isLocked(key string) bool {
 
 func (km *keyedMutex) Lock(key string) {
 	for km.isLocked(key) {
+		km.c.L.Lock()
+		// notest
 		km.c.Wait()
+		km.c.L.Unlock()
 	}
 	km.l.Lock()
 	defer km.l.Unlock()
