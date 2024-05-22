93135748fdb709a931c49ec25c1718801467b4b3
Juca Crispim <jucacrispim@gmail.com>
2024-05-22 19:32:34 -0300

    Fix docs upload
    ci: include-builders Docs

--- build-scripts/upload_docs.sh
+++ build-scripts/upload_docs.sh
@@ -4,4 +4,4 @@ cd docs/build
 mv html tupi
 tar -czf docs.tar.gz tupi
 
-curl --user "$TUPI_USER:$TUPI_PASSWD" -F 'file=@docs.tar.gz' https://docs.poraodojuca.dev/e/
+curl --user -F 'file=@docs.tar.gz' https://docs.poraodojuca.dev/e/ -H "Authorization: Key $TUPI_AUTH_KEY"
