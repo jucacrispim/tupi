56c692b30a329f414833408228daddf9d5a69d76
Juca Crispim <juca@debianmental.poraodojuca.dev>
2025-08-04 21:11:07 -0300

    pin courtney version

--- build-scripts/env.sh
+++ build-scripts/env.sh
@@ -4,8 +4,8 @@ VENV_DIR="$HOME/.virtualenvs"
 DOCS_VENV_DIR="$VENV_DIR/tupi-docs"
 install_courtney(){
     go get golang.org/x/tools@v0.25.0
-    go get github.com/dave/courtney
-    go install github.com/dave/courtney
+    go get github.com/dave/courtney@v0.4.3
+    go install github.com/dave/courtney@v0.4.3
 }
 
 setup_env(){
